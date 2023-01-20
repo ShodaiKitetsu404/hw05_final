667662d4bb6c9ba5c1f56f78f055744025dc1145
Стас Басов <stas.basov@whatever.com>
2023-01-21 00:53:36 +0800

    fix_review

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -68,7 +68,10 @@ class StaticURLTests(TestCase):
         response = StaticURLTests.guest_client.get(
             StaticURLTests.post_edit_url
         )
-        self.assertRedirects(response, StaticURLTests.post_url)
+        self.assertRedirects(
+            response,
+            f'/auth/login/?next={StaticURLTests.post_url}edit/'
+        )
 
     def test_only_authorized_can_create_post(self):
         response = StaticURLTests.authorized_client.get(
